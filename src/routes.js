8c3ff67739159af55a13e799aae1b561149f8202
GustavoSantosLima <gustavo.santos.lima@hotmail.com>
2019-07-02 19:48:01 -0300

    Criando novas tabelas e rotas e configurando o MongoDB

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,9 @@ import { Router } from 'express';
 import multer from 'multer';
 import multerConfig from './config/multer';
 import UserController from './app/controllers/UserController';
+import AppointmentController from './app/controllers/AppointmentController';
+import ProviderController from './app/controllers/ProviderController';
+import ScheduleController from './app/controllers/ScheduleController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import AuthMiddleware from './app/middlewares/AuthMiddleware';
@@ -15,6 +18,13 @@ routes.post('/sessions', SessionController.store);
 routes.use(AuthMiddleware);
 
 routes.put('/update', UserController.update);
+
+routes.get('/providers', ProviderController.index);
+
+routes.get('/appointments', AppointmentController.index);
+routes.post('/appointments', AppointmentController.store);
+
+routes.get('/schedule', ScheduleController.index);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
